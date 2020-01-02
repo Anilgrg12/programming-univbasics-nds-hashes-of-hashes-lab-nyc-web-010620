df644ac72fd9e2f3eed9fef04431c702a8f39854
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-02 23:24:13 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -37,7 +37,11 @@ def naming_system
   sub_category: {
     label: "Phylum",
   sub_category: {
-     label: "Class"
+     label: "Class",
+     sub_category: {
+       label: "Order",
+       sub_category: {
+     }
   
   
   # Remember:
